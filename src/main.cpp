4646b5985f591105b475b9a07e3e9f4bc0cf35a7
Tony Yen <67096653+TonyYenTWN@users.noreply.github.com>
2024-06-28 22:20:48 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,18 @@
 #include "src/ts.h"
 
 int main(int argc, char* argv[]){
+    time_series_class time_series;
+//    time_series.get_past_days(7);
+    auto ts_test = time_series.create_ts_test();
+
+    // Database Test
+//    data_base_class data_base;
+//    auto conn = data_base.conn_init();
+
     // Optimization Test
-    market_class market;
-    market.test();
-    scheduler_class scheduler;
-    scheduler.LP_set(market);
-    scheduler.LP_solve(market);
+//    market_class market;
+//    market.test();
+//    scheduler_class scheduler;
+//    scheduler.LP_set(market);
+//    scheduler.LP_solve(market);
 }
